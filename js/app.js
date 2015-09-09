aa6af40754dec9eab094453d36d96a36f59fa6c3
rajanand <rajanand0202@gmail.com>
2015-09-10 00:07:41 +0530

    add phaser create function, make the user image fall with some gravity effect

--- js/app.js
+++ js/app.js
@@ -5,7 +5,26 @@ var mainState = {
   preload: function () {
     game.stage.backgroundColor = '#40D954';
     game.load.image('bird', 'images/user_icon.png');
-  }
+  },
+
+  create: function () {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    this.bird = this.game.add.sprite(100, 245, 'bird');
+
+    game.physics.arcade.enable(this.bird);
+
+    this.bird.body.gravity.y = 1000;
+
+    var spaceBar = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+    spaceBar.onDown.add(this.jump, this);
+  },
+
+  update: function() {  
+    if (this.bird.inWorld === false)
+      this.restartGame();
+  },
 };
 
 
