c78e772349c3aecc2eaff4e712cc5e007db0aefe
rajanand <rajanand0202@gmail.com>
2015-09-10 00:14:41 +0530

    add fb image for pipe and create phaser group for adding pipes

--- js/app.js
+++ js/app.js
@@ -5,9 +5,12 @@ var mainState = {
   preload: function () {
     game.stage.backgroundColor = '#40D954';
     game.load.image('bird', 'images/user_icon.png');
+    game.load.image('pipe', 'images/fb.png');
   },
 
   create: function () {
+
+    //add bird/user_icon
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     this.bird = this.game.add.sprite(100, 245, 'bird');
@@ -16,6 +19,14 @@ var mainState = {
 
     this.bird.body.gravity.y = 1000;
 
+    //add pipes
+    this.pipes = game.add.group();
+
+    this.pipes.enableBody = true;
+    
+    this.pipes.createMultiple(20, 'pipe');
+
+    // add spaceBar key
     var spaceBar = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
     spaceBar.onDown.add(this.jump, this);
