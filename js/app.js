964a90e605323ccba9615d2709bb874892012e2e
rajanand <rajanand0202@gmail.com>
2015-09-10 01:12:59 +0530

    add comments for self documentation

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var game = new Phaser.Game(400, 500, Phaser.AUTO, 'gameContainer');
 
 
 var mainState = {
+  // load the images
   preload: function () {
     game.stage.backgroundColor = '#40D954';
     game.load.image('bird', 'images/user_icon.png');
@@ -17,6 +18,7 @@ var mainState = {
 
     game.physics.arcade.enable(this.bird);
 
+    //setting initial gravity to 1000
     this.bird.body.gravity.y = 1000;
 
     //add pipes
@@ -30,23 +32,30 @@ var mainState = {
     var spaceBar = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
     spaceBar.onDown.add(this.jump, this);
+
     this.timer = game.time.events.loop(1500, this.addRowOfPipes, this); 
+
+    //show initial score
     this.score = 0;
     this.labelScore  = game.add.text(20, 20, "0", {font: '25px Bold', fill: '#ffffff'})
   },
 
   update: function() {  
+    // if bird not inside the game restart it
     if (this.bird.inWorld === false){
       this.restartGame();
     }
+    // if two images overlap restart the game
     game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
   },
 
   jump: function () {
+    // setting velocity of bird
     this.bird.body.velocity.y = -350;
   },
 
   restartGame: function(){
+    // reset the game
     game.state.start('main');
   },
 
@@ -59,6 +68,7 @@ var mainState = {
   },
 
   addRowOfPipes: function () {
+    // create hole in pipe
     var hole = Math.floor(Math.random() * 5) + 1;
 
     for (var i=0; i < 8; ++i) {
@@ -66,7 +76,7 @@ var mainState = {
         this.addOnePipe(400, i*60 +10);
       }
     }
-
+    // increase the row on every successful pass
     this.score += 1;
     this.labelScore.text = this.score;
   }
